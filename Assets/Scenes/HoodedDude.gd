46a704ae686aae991b12d07c29c3ea898efa08db
Derek Coblentz <derek.coblentz@gmail.com>
2023-02-17 23:00:09 -0600

    finished ice spell sorta

--- Assets/Scenes/HoodedDude.gd
+++ Assets/Scenes/HoodedDude.gd
@@ -6,8 +6,10 @@ var velocity = Vector2.ZERO
 onready var sprite = get_node("Sprite")
 #const fireball = preload("res://Fireball.tscn")
 export(PackedScene) var fireball = preload("res://Assets/Scenes/Fireball.tscn")
+var icicle = preload("res://Assets/Scenes/Icicle.tscn")
+var icicle_ready = true
 
-func get_input():
+func get_input(delta):
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
@@ -35,11 +37,32 @@ func get_input():
 		var dir = get_global_mouse_position() - fball_origin
 		fball.init(fball_origin, dir)
 		get_parent().add_child(fball)
+	
+	if Input.is_action_pressed("rightclick", true) and icicle_ready:
+		icicle_ready = false
+		var icicle_timer = Timer.new()
+		icicle_timer.wait_time = 0.2
+		icicle_timer.autostart = true
+		add_child(icicle_timer)
+		icicle_timer.connect("timeout", self, "ice_timeout")
+		
+		if mana == 0:
+			return
+		#mana -= 1
+		$Control/ManaBar.update_mana(mana)
+		var ice_origin = $IcicleOrigin.global_position
+		var ice = icicle.instance()
+		var dir = get_global_mouse_position() - ice_origin
+		ice.init(ice_origin, dir)
+		get_parent().add_child(ice)
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	velocity = move_and_slide(velocity)
 
+
+func ice_timeout():
+	icicle_ready = true
 
 func _on_Timer_timeout():
 	mana += 1
---
+++ Assets/Scenes/Icicle.gd
@@ -0,0 +1,49 @@
+extends Area2D
+
+var rng = RandomNumberGenerator.new()
+var ice1 = preload("res://Assets/Sprites/Icicle1.png")
+var ice2 = preload("res://Assets/Sprites/Icicle2.png")
+var ice3 = preload("res://Assets/Sprites/Icicle3.png")
+var ices = [ice1, ice2, ice3]
+
+#var ice1sound = preload("res://Assets/Sounds/iceshard1.wav")
+#var ice2sound = preload("res://Assets/Sounds/iceshard2.wav")
+#var ice3sound = preload("res://Assets/Sounds/iceshard3.wav")
+#var sounds = [ice1sound, ice2sound, ice3sound]
+
+var speed = 150
+var velocity = Vector2.ZERO
+var maxtime = 4.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+	var rint = rng.randi_range(0, 2)
+	$Sprite.texture = ices[rint]
+	#$AudioStreamPlayer2D.stream = sounds[rint]
+	#$AudioStreamPlayer2D.autoplay = true
+	var timer = Timer.new()
+	timer.wait_time = maxtime
+	timer.autostart = true
+	add_child(timer)
+	timer.connect("timeout", self, "on_timeout")
+
+func _process(delta):
+	position += velocity * delta
+	speed += 5
+
+func init(spawnspot, direction):
+	rng.randomize()
+	var rint = rng.randi_range(-60, 60)
+	global_position.x = spawnspot.x + rint
+	global_position.y = spawnspot.y
+	velocity = direction.normalized() * speed
+	rotation = get_angle_to(to_global(direction)) + 200
+
+func on_timeout():
+	self.queue_free()
+
+
+func _on_Icicle_body_entered(body):
+	body.takeDamage(30)
+	self.queue_free()
