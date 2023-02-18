3da862dd1c47f872e5648c485242e93a45cb22ff
Levi Koger <levikoger@gmail.com>
2023-02-17 22:21:21 -0600

    Hide trail

--- Assets/Scenes/HoodedDude.gd
+++ Assets/Scenes/HoodedDude.gd
@@ -7,6 +7,14 @@ onready var sprite = get_node("Sprite")
 #const fireball = preload("res://Fireball.tscn")
 export(PackedScene) var fireball = preload("res://Assets/Scenes/Fireball.tscn")
 
+func update_flip(flipped):
+	sprite.flip_h = flipped
+	if flipped:
+		$Trail.scale.x = -1
+	else:
+		$Trail.scale.x = 1
+	
+
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
@@ -21,9 +29,9 @@ func get_input():
 	velocity = velocity.normalized() * speed
 	
 	if (get_global_mouse_position() - position).x >= 0:
-		sprite.flip_h = false
+		update_flip(false)
 	else:
-		sprite.flip_h = true
+		update_flip(true)
 	
 	if Input.is_action_just_pressed("leftclick", true):
 		if mana == 0:
